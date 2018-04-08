787e9d9596c3ce5c25e420fe5dd4f82eda43c598
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2018-04-08 14:15:03 +0800

    xxx

--- oauth2-server/oauth2-service/src/main/java/com/cloud/oauth2/service/role/SysRoleModuleService.java
+++
@@ -1,8 +0,0 @@
-package com.cloud.oauth2.service.role;
-
-/***
- * 角色资源 service
- */
-public interface SysRoleModuleService {
-
-}
--- oauth2-server/oauth2-service/src/main/java/com/cloud/oauth2/service/role/SysRoleService.java
+++
@@ -1,8 +0,0 @@
-package com.cloud.oauth2.service.role;
-
-/***
- * 角色 service
- */
-public interface SysRoleService {
-
-}
